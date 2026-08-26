6f8eb09194aa612cde2e98080cea96babfc21af7
aldoambriz8551 <aldo.ambriz8551@alumnos.udg.mx>
2026-08-26 08:20:50 -0600

    mi primer commit

--- gui}.py
+++ gui}.py
@@ -6,7 +6,7 @@ import tkinter as tk
 def saludar():
     nombre = entrada.get().strip()
     if not nombre:
-        nombre = "amigo"
+        nombre = "aldo ambriz"
     lbl.config(text=f"Hola {nombre}!")
 
 
